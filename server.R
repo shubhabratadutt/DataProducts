cb28d2f355db55b56c643979e17f4a4a9956e6b9
shubhabratadutt <shubhabratadutt@yahoo.com>
2015-07-26 10:27:18 -0400

    First commit

---
+++ server.R
@@ -0,0 +1,46 @@
+library(shiny)
+library(datasets)
+data(ToothGrowth)
+
+# Define server logic for random distribution application
+shinyServer(function(input, output) {
+      
+  # generate the plot
+  output$plot <- renderPlot({
+    validate(
+      need(input$mode, 'Check at least one mode'),   
+      need(input$dose %in% c(0.5,1,2), 'Please select a valid range')
+    )
+    doseR <- seq(input$dose[1],input$dose[2], 0.5)
+    subset <- ToothGrowth[with(ToothGrowth, supp %in% input$mode & dose %in% doseR),]
+    x <- input$xAxis
+    xlabel <- if(x=="supp") "mode" else "dose"
+    subset$dose <- as.factor(subset$dose)
+    plot(if(x=="supp") subset$supp else subset$dose, subset$len, 
+         main = paste("Boxplot of length vs. ",xlabel),
+         xlab = xlabel, ylab = "length")    
+  })
+  
+  
+  # Generate a summary of the data
+  output$summary <- renderPrint({
+    validate(
+      need(input$mode, 'Check at least one mode'),
+      need(input$dose %in% c(0.5,1,2), 'Please select a valid range')
+    )
+    doseR <- seq(input$dose[1],input$dose[2], 0.5)
+    subset <- ToothGrowth[with(ToothGrowth, supp %in% input$mode & dose %in% doseR),]
+    summary(subset)
+  })
+  
+  # Generate an HTML table view of the data
+  output$table <- renderTable({
+    validate(
+      need(input$mode, 'Check at least one mode'),
+      need(input$dose %in% c(0.5,1,2), 'Please select a valid range')
+    )
+    doseR <- seq(input$dose[1],input$dose[2], 0.5)
+    ToothGrowth[with(ToothGrowth, supp %in% input$mode & dose %in% doseR),]    
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,57 @@
+library(shiny)
+
+# Define UI for random distribution application 
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("ToothGrowth"),
+  
+  # Sidebar with controls to select the random distribution type
+  # and number of observations to generate. Note the use of the
+  # br() element to introduce extra vertical spacing
+  sidebarLayout(
+    sidebarPanel(
+      checkboxGroupInput("mode", "Mode of Delivery:",
+                   c("Orange Juice" = "OJ",
+                     "Ascorbic Acid" = "VC"),
+                   selected = c("OJ","VC")),
+      br(),
+      
+      sliderInput("dose", 
+                  "Dose:", 
+                  min = 0.5, 
+                  max = 2.0,
+                  step = 0.5,
+                  value = c(0.5,2)),
+      br(),
+      
+      radioButtons("xAxis", "X - Axis", c("Mode of delivery"="supp", 
+                                          "Dose"="dose"), selected="dose"),
+      br(),
+      h4("Usage Instructions"),
+      p("Use this tool to visualize the effect of the",
+        span(" dose ", style = "color:blue"), "and the ",
+        span("mode of delivery", style = "color:blue"), " of ",
+        span("vitamin C", style = "color:blue"), " on the lenght of odontoblasts ",
+        span("(teeth)", style = "color:blue"), " of 10 guniea pigs."),
+      p("There are three dose levels: 0.5, 1 and 2. Use the slider to select one or
+      more of these levels."),
+      p("Select the predictor of tooth length using the redio buttons for X-Axis.
+        The resulting plot will show the effect of the predictor on tooth length."),
+      p("The ",
+        span("Summary", style = "color:blue"), " of the data subset and a ",
+        span("Table", style = "color:blue"),  " view of the same are 
+        available in the respoctive tabs in the main panel")
+    ),
+    
+    # Show a tabset that includes a plot, summary, and table view
+    # of the generated distribution
+    mainPanel(
+      tabsetPanel(type = "tabs", 
+                  tabPanel("Plot", plotOutput("plot")), 
+                  tabPanel("Summary", verbatimTextOutput("summary")), 
+                  tabPanel("Table", tableOutput("table"))
+      )
+    )
+  )
+))
